9b36afb6e5b90d9412958c7ece6e98e07c9a62a9
ObidovSamandar <samandarobidovfrd@gmail.com>
2021-04-01 00:40:32 +0500

    Update about.js

--- about.js
+++ about.js
@@ -3,25 +3,25 @@ $(document).ready(()=>{
         delay:950,
         time:4100
     })
- })
-
-
-let skillBoxesList = $('.skill__box');
- let skillBoxPercentages = [65,60,45,50,55,45]
-$.each(skillBoxesList, (i, val)=>{
-    let newSkillBox = new ldBar("#"+val.id,{
-     "preset": "circle",
-     "value": 0,
-     "stroke": "#FFB400",
-     "data-present":"circle",
-     "stroke-width":8,
-     "stroke-trail-width":8,
-     "stroke-trail":"#252525",
+            
+    let skillBoxesList = $('.skill__box');
+    let skillBoxPercentages = [65,60,45,50,55,45]
+    $.each(skillBoxesList, (i, val)=>{
+        let newSkillBox = new ldBar("#"+val.id,{
+        "preset": "circle",
+        "value": 0,
+        "stroke": "#FFB400",    
+        "data-present":"circle",
+        "stroke-width":8,
+        "stroke-trail-width":8,
+        "stroke-trail":"#252525",
+        })
+        newSkillBox.set(skillBoxPercentages[i],true,)
     })
-    newSkillBox.set(skillBoxPercentages[i],true,)
+
+    AOS.init({
+        delay:900,
+        once: true
+    })
 })
 
-AOS.init({
-    delay:900,
-    once: true
-})
