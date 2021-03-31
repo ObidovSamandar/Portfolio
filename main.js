0d6c7d78c95056aad30d59309c0f8cb981909f3c
ObidovSamandar <samandarobidovfrd@gmail.com>
2021-03-31 17:50:46 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,38 @@
+$(document).ready(()=>{
+    
+    // ===HAMBURGER BUTTON===
+    $('.hamburger__bar').on('click',()=>{
+        $('.ham__bars:nth-child(1),.ham__bars:nth-child(3),.ham__bars:nth-child(2),.res__header').toggleClass('active')
+        console.log();
+        if($('.res__header').attr('class')=='res__header active'){
+            disableScroll()
+        }else{
+            enableScroll()
+        }
+    
+    })
+    
+
+})
+function disableScroll() {
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+        window.onscroll = function() {
+            window.scrollTo(scrollLeft, scrollTop);
+        };
+}
+
+function enableScroll() {
+    window.onscroll = function() {};
+}
+
+
+
+var div=document.createElement("div");
+div.id="preloader";
+div.className="preloader";
+div.innerHTML='<div class="black_wall"></div><div class="loader"></div>';
+document.body.insertBefore(div,document.body.firstChild);
+
+
+window.onload=function(){document.getElementById("preloader").classList.add("off")};
